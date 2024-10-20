7869fc487f6aff621636f2b01f838889ad9e7169
BK-beep <baraka.khaoula0@gmail.com>
2024-10-20 16:16:13 +0100

    initial

--- src/main/java/ma/enset/accountservice/AccountServiceApplication.java
+++ src/main/java/ma/enset/accountservice/AccountServiceApplication.java
@@ -21,6 +21,7 @@ public class AccountServiceApplication {
     @Bean
     CommandLineRunner commandLineRunner(BankAccountRepo bankAccountRepo){
         return args -> {
+
             for (int i = 0; i < 10; i++) {
                 BankAccount bankAccount = BankAccount.builder()
                         .id(UUID.randomUUID().toString())
